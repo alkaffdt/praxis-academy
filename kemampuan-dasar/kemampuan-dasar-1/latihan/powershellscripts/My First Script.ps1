97fa75ad5a0df5ee29658d46f2c0e5ffc1c522a6
alkaffdt <alkaffdt3@gmail.com>
2019-10-24 20:19:13 +0700

    upload latihan 1 (update)

---
+++ kemampuan-dasar/kemampuan-dasar-1/latihan/powershellscripts/My First Script.ps1
@@ -0,0 +1,22 @@
+﻿###########################################################
+
+$Path = "D:\praxis-academy\praxis-academy"
+$Text = "This is the data that I am looking for"
+$PathArray = @()
+$Results = "C:\temp\test.txt"
+
+
+# This code snippet gets all the files in $Path that end in ".txt".
+Get-ChildItem $Path -Filter "*.java" Recurse |
+Where-Object { $_.Attributes -ne "Directory"} |
+ForEach-Object {
+If (Get-Content $_.FullName | Select-String -Pattern $Text) {
+$PathArray += $_.FullName
+$PathArray += $_.FullName
+}
+}
+Write-Host "Contents of ArrayPath:"
+$PathArray | ForEach-Object {$_}
+
+
+##########################################################
