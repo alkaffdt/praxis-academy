09391509ad13aac16f97e3f5626d039ad486579e
Alkaff <alkaffdt3@gmail.com>
2019-11-14 18:46:03 +0700

    up malam

--- minggu-2/hari-4/animator_test/lib/main.dart
+++ minggu-2/hari-4/animator_test/lib/main.dart
@@ -3,22 +3,23 @@ import 'package:animator/animator.dart';
 
 void main() => runApp(AnimatedLogo());
 
-// class AnimatedLogo extends StatelessWidget{
-//   Widget build(BuildContext context){
-//     return Animator<double>(
-//       tween: Tween<double>(
-//         begin: 0,
-//         end: 300),
-//       cycles: 0,
-//       builder: (anim) => Center(
-//         child: Container(
-//           margin: EdgeInsets.symmetric(vertical: 10),
-//           height: anim.value,
-//           width: anim.value,
-//           child: FlutterLogo(),
-//         ),
-//       ),
-//     );
-//   }
-// }
+class AnimatedLogo extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Animator<double>(
+      tween: Tween<double>(
+        begin: 0,
+        end: 370,),
+      duration: Duration(seconds: 5),
+      cycles: 3,
+      builder: (anim) => Center(
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 50),
+          height: anim.value,
+          width: anim.value,
+          child: FlutterLogo(),
+        ),
+      ),
+    );
+  }
+}
 
